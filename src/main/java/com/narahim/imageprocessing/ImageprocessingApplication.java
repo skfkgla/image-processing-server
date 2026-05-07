1c935cb4261aa99f9dd139c4dbe736e9dc8f1ae9
skfkgla <narahim.lee@gmail.com>
2026-05-07 19:05:19 +0900

    first commit

---
+++ src/main/java/com/narahim/imageprocessing/ImageprocessingApplication.java
@@ -0,0 +1,13 @@
+package com.narahim.imageprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageprocessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/narahim/imageprocessing/ImageprocessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narahim.imageprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageprocessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
